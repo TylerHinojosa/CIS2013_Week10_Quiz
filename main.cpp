3269be3fc9d0aaa15567b717bd99b17260555f22
TylerHinojosa <46981261+TylerHinojosa@users.noreply.github.com>
2019-03-27 17:56:29 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream> 
+#include <string>
+using namespace std;
+
+int main(){
+	
+	int cnt = 0;
+	string sentence;
+	
+	cout << "Type your sentence:" << endl;
+	
+	getline(cin, sentence);
+	
+	for(int x = 0; x <= sentence.length(); x++){
+		if(sentence[x] == 'b'){
+			cnt++;
+			if(cnt%2 == 1){
+				cout << "SIT";
+			} else {
+					cout << "STAND";
+			}
+		} else {
+				cout << sentence[x];
+		}
+	}
+	
+}
